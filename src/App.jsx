3a18b0a28b9f923e01733added454885fac0c02c
Shariful Islam <sajukhan12905@gmail.com>
2024-09-22 22:00:44 +0600

    before registration

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Toastify from "./Toastify";
 
 function App() {
   const user = true;
+
   return (
     <div className=" container w-[90vw] h-[90vh] bg-gray-800/90  text-white border mt-7 border-white rounded-md  flex  ">
       {user ? (
